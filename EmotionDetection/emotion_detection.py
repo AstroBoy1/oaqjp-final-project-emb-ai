494b479e96904b5492282eb27869cc2b3c8cfb21
astroboy1 <omorim@oregonstate.edu>
2025-03-15 17:10:13 -0400

    error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,6 +5,14 @@ def emotion_detector(text_to_analyse):
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, headers=headers, json=input_json)
+    status_code = response.status_code
+    if status_code == 400:
+        return {'anger': None,
+        'disgust': None,
+        'fear': None,
+        'joy': None,
+        'sadness': None,
+        'dominant_emotion': None}
     text = response.json()
     anger_score = text["emotionPredictions"][0]["emotion"]["anger"]
     disgust_score = text["emotionPredictions"][0]["emotion"]["disgust"]
--- server.py
+++ server.py
@@ -11,6 +11,8 @@ def index():
 def analyze_emotion():
     text_to_analyze = request.args.get('textToAnalyze', '')
     result = emotion_detector(text_to_analyze)
+    if not result["dominant_emotion"]:
+        return "Invalid text! Please try again!"
     anger, disgust, fear, joy, sadness, dominant = result["anger"], result["disgust"], result["fear"], result["joy"], result["sadness"], result["dominant_emotion"]
     formatted = "For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. The dominant emotion is {dominant}."
     return formatted.format(anger=anger, disgust=disgust, fear=fear, joy=joy, sadness=sadness, dominant=dominant)
